1268d944738d376ead8f32bfda28a75f7da1af12
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 00:50:57 -0600

    Added saved_artifacts method

--- server/util.py
+++ server/util.py
@@ -1,5 +1,11 @@
+#reads the columns.json
 def get_location_names():
     pass
+
+def load_saved_artifacts:
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
 
 if __name__ == '__main__':
     print(get_location_names())
