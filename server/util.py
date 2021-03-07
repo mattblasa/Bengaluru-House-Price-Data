d27f5c8fb605b1800f56ec80ec65a9e1997211be
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 13:12:43 -0600

    Fixed X variable exception
    missing variable.

--- server/util.py
+++ server/util.py
@@ -14,8 +14,7 @@ def get_estimated_price(location, sqft, bhk, bath):
     except: 
         loc_index = -1 
 
-    loc_index = np.where(X.columns == location)[0][0]
-    x = np.zeros() 
+    x = np.zeros(len(__data_columns)) 
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
@@ -35,7 +34,7 @@ def load_saved_artifacts():
     with open("./artifacts/columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
-
+    global __model
     with open('./artifacts/bangalore_price_model.pickle', 'rb') as f:
         __model = pickle.load(f)
     print('Loading Artifacts....completed')
@@ -43,5 +42,8 @@ def load_saved_artifacts():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-   
-    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))
+    print(get_estimated_price('Ejipura', 1000, 2, 2))
+    #print(get_location_names())
