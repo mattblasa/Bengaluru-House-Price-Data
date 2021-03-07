620bafd858c4afdf3474d39ca3365803ec3b4eac
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 00:44:27 -0600

    Update server.py

--- server/server.py
+++ server/server.py
@@ -2,11 +2,16 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
     return "Hi"
-
-
 
 if __name__ == '__main__':
     print("Starting Python Flask Server for Home Price Prediction...")
