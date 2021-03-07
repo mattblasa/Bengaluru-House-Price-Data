0a30345ab7471293edc78ce2db74c21656e33d68
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 00:12:59 -0600

    Added artifacts

--- server/server.py
+++ server/server.py
@@ -1,9 +1,12 @@
+#local flask server
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
     return "Hi"
+
+
 
 if __name__ == '__main__':
     print("Starting Python Flask Server for Home Price Prediction...")
