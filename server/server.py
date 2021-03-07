f769fa231c7b8458e003febca7f066dd9ff6e3e8
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 13:30:23 -0600

    Add prediction method

--- server/server.py
+++ server/server.py
@@ -13,9 +13,19 @@ def get_location_names():
 
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+#create another end point
+@app.route('/predict_home_price', methods=['POST']) #Check GET and POST 
 def predict_home_price():
-    pass
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+
+    return response
 
 if __name__ == '__main__':
     print("Starting Python Flask Server for Home Price Prediction...")
