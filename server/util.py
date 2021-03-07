f0424e4b823f4cb2dede6f20b00dec8efd723ae9
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 01:01:24 -0600

    Update util.py

--- server/util.py
+++ server/util.py
@@ -1,12 +1,13 @@
 #reads the columns.json
 import json 
+import pickle
 
 __locations = None
 __data_columns = None
 __model = None 
 
 def get_location_names():
-    pass
+    return __locations
 
 def load_saved_artifacts:
     print("loading saved artifacts...start")
@@ -16,6 +17,10 @@ def load_saved_artifacts:
     with open("./artifacts/columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
+
+    with open('./artifacts/bangalore_price_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    print('Loading Artifacts....completed')
 
 if __name__ == '__main__':
     print(get_location_names())
