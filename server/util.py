f78213c319c5132731f57999b06fa5d511bf511d
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 00:57:28 -0600

    Update util.py

--- server/util.py
+++ server/util.py
@@ -1,4 +1,10 @@
 #reads the columns.json
+import json 
+
+__locations = None
+__data_columns = None
+__model = None 
+
 def get_location_names():
     pass
 
@@ -6,6 +12,10 @@ def load_saved_artifacts:
     print("loading saved artifacts...start")
     global __data_columns
     global __locations
+
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
 
 if __name__ == '__main__':
     print(get_location_names())
